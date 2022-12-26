ce7d6b88f88490d86a307d62f9f6d675f6858b0f
Kirill Gusakov <justmymail003@gmail.com>
2022-12-26 14:40:03 +0300

    e

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
         String max;
-        max = first >= second ? "first" : "second";
+        max = first > second ? "first" : "second";
         System.out.println(max);
     }
 }
